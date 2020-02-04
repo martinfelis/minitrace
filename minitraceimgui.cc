3770ff7214e3426ccea7547096f60fe4988ad4d9
Martin Felis <martin@fysx.org>
2020-02-04 22:41:56 +0100

    Cleaned up app

--- minitraceimgui.cc
+++ minitraceimgui.cc
@@ -6,6 +6,8 @@
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 
+#include "minitrace.h"
+
 static void glfw_error_callback(int error, const char* description)
 {
     fprintf(stderr, "Glfw Error %d: %s\n", error, description);
@@ -84,7 +86,10 @@ int main (int argc, char* argv[]) {
 	// Our state
 	bool show_demo_window = true;
 	bool show_another_window = false;
+	bool show_profiler_window = true;
 	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+	mtr_init("trace.json");
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
@@ -101,40 +106,19 @@ int main (int argc, char* argv[]) {
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
 
-		// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-		if (show_demo_window)
-			ImGui::ShowDemoWindow(&show_demo_window);
+		if (ImGui::BeginMainMenuBar()) {
+			ImGui::Checkbox("Profiler", &show_profiler_window);
 
-		// 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
-		{
-			static float f = 0.0f;
-			static int counter = 0;
 
-			ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-			ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-			ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-			ImGui::Checkbox("Another Window", &show_another_window);
-
-			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-			ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-			if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-				counter++;
-			ImGui::SameLine();
-			ImGui::Text("counter = %d", counter);
-
-			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-			ImGui::End();
+      ImGui::EndMainMenuBar();
 		}
 
-		// 3. Show another simple window.
-		if (show_another_window)
-		{
-			ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-			ImGui::Text("Hello from another window!");
-			if (ImGui::Button("Close Me"))
-				show_another_window = false;
+
+		if (show_profiler_window) {
+			ImGui::Begin("Profiler", &show_profiler_window);
+#ifdef MTR_ENABLED
+			mtr_imgui_draw();
+#endif
 			ImGui::End();
 		}
 
@@ -149,6 +133,8 @@ int main (int argc, char* argv[]) {
 
 		glfwSwapBuffers(window);
 	}
+
+	mtr_shutdown();
 
 	// Cleanup
 	ImGui_ImplOpenGL3_Shutdown();
